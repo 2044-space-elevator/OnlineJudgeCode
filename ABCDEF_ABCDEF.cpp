c1fddcce4ea3585baa55363c3f8c6998adac4207
2044-space-elevator <Rainwang_20220102@163.com>
2023-09-06 18:28:16 +0800

    2023/09/06

---
+++ ABCDEF_ABCDEF.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+int n;
+#define rep(a) for(int a=1;a<=n;a++)
+using namespace std;
+
+unordered_map<int, int> dict;
+int arr[105];
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+    rep(i) rep(j) rep(k) {
+        dict[(arr[i] + arr[j]) * arr[k]]++;
+    }
+    int ans = 0;
+    rep(i) rep(j) rep(k) {
+        ans += dict[arr[i] * arr[j] + arr[k]];
+    }
+    cout << ans;
+}

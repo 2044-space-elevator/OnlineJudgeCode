f481c658cace269e9d4cb0334c5e5f360bd37b3f
2044-space-elevator <spaceelevator2044@126.com>
2024-12-07 20:20:57 +0800

    2024/12/07

--- Luogu/P_3372_模板_线段树_1.cpp
+++ Luogu/P_3372_模板_线段树_1.cpp
@@ -10,98 +10,84 @@
 #define stg string
 #define vct vector
 using namespace std;
-#define int long long
-
-const int N = 1e6 + 5;
-int n, t, arr[N], tag[N], ope[N];
 
 typedef long long ll;
 typedef unsigned long long ull;
 
-namespace LinkTree {
-    void build(int now, int left, int right) {
-        if (left == right) {
-            ope[now] = arr[left];
-            return;
-        }
-        int mid = (left + right) >> 1;
-        build(now * 2, left, mid);
-        build(now * 2 + 1, mid + 1, right);
-        ope[now] = ope[now * 2] + ope[now * 2 + 1];
-    }
-    void update(
-        int begin, int end, int left, 
-        int right, int now, int gs
-    ) {
-        if (begin >= left && right >= end){
-            ope[now] += gs * (end - begin + 1);
-            tag[now] += gs;
-            return;
-        }
-        int mid = (begin + end) >> 1;
-        tag[now * 2] += tag[now];
-        ope[now * 2] += tag[now] * (mid - begin + 1);
-        tag[now * 2 + 1] += tag[now];
-        ope[now * 2 + 1] += tag[now] * (end - mid);
-        tag[now] = 0;  
-        if (left <= mid) {
-            update(begin, mid, left, right, now * 2, gs);
-        }
-        if (right > mid) {
-            update(mid + 1, end, left, right, now * 2 + 1, gs);
-        }
-        ope[now] = ope[now * 2] + ope[now * 2 + 1];
-    }
-    int query(
-        int begin, int end, int left,
-        int right, int now
-    ) {
-        int res = 0;
-        if (left >= begin && right <= end) {
-            return ope[now];
-        }
-        int mid = (left + right) >> 1;
-        tag[now * 2] += tag[now];
-        ope[now * 2] += tag[now] * (mid - left + 1);
-        tag[now * 2 + 1] += tag[now];
-        ope[now * 2 + 1] += tag[now] * (right - mid);
-        tag[now] = 0;  
-        int sum = 0;
-        if (begin <= mid) {
-            sum += query(begin, end, left, mid, now * 2);
-        }
-        if (end > mid) {
-            sum += query(begin, end, mid + 1, right, now * 2 + 1);
-        }
-        return sum;
-    }
-
-};
-
-using namespace LinkTree;
-
 void solve() {
-    int typ;
-    cin >> typ;
-    if (typ == 1) {
-        int x, y, k;
-        cin >> x >> y >> k;
-        update(1, n, x, y, 1, k);
-    } else {
-        int x, y;
-        cin >> x >> y;
-        cout << query(x, y, 1, n, 1) << endl;
-    }
+	
+}
+const int N = 1E5 + 5;
+
+#define int ll
+struct Segment {
+  int l, r, data, lazy;
+}tree[N * 4];
+
+int arr[N];
+void pushUp(int i) {
+  tree[i].data = tree[i * 2].data + tree[i * 2 + 1].data;
+}
+void pushDown(int i) {
+  if (tree[i].lazy) {
+    int mid = (tree[i].l + tree[i].r) >> 1;
+    tree[i * 2].data += tree[i].lazy * (mid - tree[i].l + 1);
+    tree[i * 2].lazy += tree[i].lazy;
+    tree[i * 2 + 1].data += tree[i].lazy * (tree[i].r - mid);
+    tree[i * 2 + 1].lazy += tree[i].lazy;
+    tree[i].lazy = 0;
+  }
 }
 
+void build(int p, int l, int r) {
+  tree[p].l = l;
+  tree[p].r = r;
+  if (l == r) {
+    tree[p].data = arr[l];
+    return;
+  }
+  int mid = (l + r) >> 1;
+  build(p * 2, l, mid);
+  build(p * 2 + 1, mid + 1, r);
+  pushUp(p);
+}
 
+void update(int p, int l, int r, int k) {
+  if (l <= tree[p].l && r >= tree[p].r) {
+    tree[p].data += (tree[p].r - tree[p].l + 1) * k;
+    tree[p].lazy += k;
+    return;
+  }
+  pushDown(p);
+  int mid = (tree[p].l + tree[p].r) >> 1;
+  if (l <= mid) update(p * 2, l, r, k);
+  if (r > mid) update(p * 2 + 1, l, r, k);
+  pushUp(p);
+}
+
+int query(int p, int l, int r) {
+  if (l <= tree[p].l && r >= tree[p].r) return tree[p].data;
+  pushDown(p);
+  int sum = 0;
+  int mid = (tree[p].l + tree[p].r) >> 1;
+  if (l <= mid) sum += query(p * 2, l, r);
+  if (r > mid) sum += query(p * 2 + 1, l, r);
+  return sum;
+}
 main() {
 //	int t; cin >> t; while (t--) solve();
-    cin >> n >> t;
-    rep(i, 1, n) {
-        cin >> arr[i];
+  int n, m; cin >> n >> m;
+  rep(i, 1, n) cin >> arr[i]; build(1, 1, n);
+  while (m--) {
+    int op, l, r;
+    cin >> op >> l >> r;
+    if (op == 1) {
+      int k;
+      cin >> k;
+      update(1, l, r, k);
+    } else {
+      cout << query(1, l, r) << endl;
     }
-    build(1, 1, n);
-    while (t--) solve();
+  }
 	return 0;
 }
